8fda3a232def2f4f405dc5d73ea46ad5d41477bf
Irã Guimarães <iranmcg016@gmail.com>
2026-05-24 19:58:36 -0300

    Inclui sinopse com text blocks

--- aulaFundamentos.java
+++ aulaFundamentos.java
@@ -5,9 +5,19 @@ public class aulaFundamentos {
 
         //Ano de lançamento do filme e se está incluso no plano do usuário
         int anoDeLancamento = 2022;
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Ano de lançamento: " + anoDeLancamento + "\n");
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
 
+        //Inclusão de sinopse com Text Blocks (""")
+        String sinopse;
+        sinopse = """
+                Filme Top Gun: Maverick
+                Filme de ação com galã dos anos 80
+                Muito bom!
+                Ano de lançamento: """ + anoDeLancamento;
+        System.out.println(sinopse);
     }
 }
+
+
