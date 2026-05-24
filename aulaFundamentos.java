726d1b6090ebfe1cd0a12153093e7bcb6391aec7
Irã Guimarães <iranmcg016@gmail.com>
2026-05-24 19:16:07 -0300

    Define ano de lançamento do filme

--- aulaFundamentos.java
+++ aulaFundamentos.java
@@ -1,5 +1,11 @@
 public class aulaFundamentos {
     public static void main (String [] args){
-        System.out.println("Alô, mundo!");
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        //Ano de lançamento do filme e se está incluso no plano do usuário
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
     }
 }
