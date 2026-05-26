079c27427c958b69c5709d7650c6420f6b0128f4
Irã Guimarães <iranmcg016@gmail.com>
2026-05-26 01:09:36 -0300

    Cria primeira interação com teclado

--- aulaLeituraDados.java
+++ aulaLeituraDados.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class aulaLeituraDados {
     public static void main(String [] args) {
         Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito: ");
+        String filmeFavorito =  leitura.nextLine();
+        System.out.println(filmeFavorito);
     }
 }
