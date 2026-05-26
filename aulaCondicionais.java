74d7fdae29a38916ea3e5d7a2ce47c207f1d5f76
Irã Guimarães <iranmcg016@gmail.com>
2026-05-26 00:48:11 -0300

    Cria arquivo de aula de condicionais alura

---
+++ aulaCondicionais.java
@@ -0,0 +1,12 @@
+public class aulaCondicionais {
+    public static void main(String[] args) {
+
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        if (anoDeLancamento > 2022){
+            System.out.println("Lançamento que os clientes estão curtindo");
+        }
+    }
+}
