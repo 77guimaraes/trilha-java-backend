1d2d59cc4cadcb49261c3b0918e056ad6fbb6ac1
Irã Guimarães <iranmcg016@gmail.com>
2026-05-26 22:55:34 -0300

    Cria arquivo de loop

---
+++ aulaLoop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class aulaLoop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner (System.in);
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+        
+        
+        
+        //Pede avaliação do filme
+        System.out.println("Diga sua avaliação sobre o filme: ");
+        double avaliacao = leitura.nextDouble();
+
+    }
+}
