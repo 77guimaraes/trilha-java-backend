1f552bb0b2d893d7a6dce4be4cb4f28139160289
Irã Guimarães <iranmcg016@gmail.com>
2026-05-26 21:55:06 -0300

    Organiza os scripts com comentarios

--- aulaLeituraDados.java
+++ aulaLeituraDados.java
@@ -4,8 +4,14 @@ public class aulaLeituraDados {
     public static void main(String [] args) {
         Scanner leitura = new Scanner(System.in);
 
+        //Pede e printa filme favorito
         System.out.println("Digite seu filme favorito: ");
         String filmeFavorito =  leitura.nextLine();
         System.out.println(filmeFavorito);
+
+        //Pede ano de lançamento
+        System.out.println("Quero saber o ano de lançamento");
+        int anoLancamento = leitura.nextInt();
+        System.out.println(anoLancamento);
     }
 }
