92b33c0264b07602cfdbad6e327fe66217ea1e6a
Irã Guimarães <iranmcg016@gmail.com>
2026-05-27 21:24:55 -0300

    Adiciona variáveis e laço de repetição de tentativas

--- jogoDeAdivinhacao.java
+++ jogoDeAdivinhacao.java
@@ -4,7 +4,19 @@ import java.util.Random;
 public class jogoDeAdivinhacao {
     public static void main(String[] args) {
 
-        Scanner leitor = new Scanner(System.in);
+        System.out.println("=== TESTE DE ADIVINHAÇÃO DE NÚMERO ===");
 
+        Scanner leitor = new Scanner(System.in);
+        int numeroGerado = new Random().nextInt(100); //gera numero aleatório entre 0 e 100
+        int tentativas = 0;
+        int numeroDigitado = 0;
+
+        while (tentativas < 5) {
+            System.out.println("Informe um numero entre 0 e 100: ");
+            numeroDigitado = leitor.nextInt();
+            tentativas++;
+
+            }
+        }
     }
 }
