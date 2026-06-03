58b9141541e0a6a43520b184f97b52a62233fef2
Irã Guimarães <iranmcg016@gmail.com>
2026-06-02 22:32:35 -0300

    Cria arquivo Titulo para ser a referencia de Filme e Serie em caso de Heranças e cola todos os codigos que estavam em Filme.java

--- poo/br/com/alura/screenmatch/modelos/Titulo.java
+++ poo/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,4 +1,51 @@
 package poo.br.com.alura.screenmatch.modelos;
 
 public class Titulo {
+    private String nome = "Top Gun";
+    private int anoLancamento = 2022;
+    private boolean incluidoNoPlano = true;
+    private double somaDasAvaliacoes;
+    private int totalAvaliacoes;
+    private int duracaoEmMinutos;
+
+
+    //Metodo acessor
+    public int getTotalAvaliacoes() {
+        return totalAvaliacoes;
+    }
+
+    //PERMITE QUE UM USUÁRIO INCLUA UM NOME
+    public void setNome(String nome) {
+        this.nome = nome; //
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    //Metodo é a maneira de algo. Nesse caso, é a maneira de exibir ficha tecnica
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoLancamento);
+    }
+
+    //Avalia notas
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    //Calculo das Medias
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalAvaliacoes;
+        //Metodos com valores numericos devem possuir um "return"
+    }
 }
