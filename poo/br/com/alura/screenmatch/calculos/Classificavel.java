ebaaf7c740a952b248d1bbdf38593e39d1912352
Irã Guimarães <iranmcg016@gmail.com>
2026-06-05 11:16:16 -0300

    Insere comentário de explicação sobre a interface

--- poo/br/com/alura/screenmatch/calculos/Classificavel.java
+++ poo/br/com/alura/screenmatch/calculos/Classificavel.java
@@ -1,5 +1,10 @@
 package poo.br.com.alura.screenmatch.calculos;
 
+/*
+Declara métodos para serem importados por classes.
+As classes que importam interfaces são obrigadas a utilizarem todos os métodos declarados na interface
+ */
+
 public interface Classificavel {
     int getClassificacao();
 }
