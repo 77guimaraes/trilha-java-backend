34f9a4f48589ca6413802b917805206a8b1810cd
Irã Guimarães <iranmcg016@gmail.com>
2026-06-13 22:40:20 -0300

    - Alteração do ArrayList de Filme para Titulo, permitindo agrupar Filmes e Séries na mesma coleção. - Uso do método avalia() para definir notas aos objetos instanciados. - Criação de laço for para iterar sobre a lista, imprimindo nomes e fazendo cast para exibir a classificação.

--- poo/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ poo/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -2,22 +2,34 @@ package poo.br.com.alura.screenmatch.principal;
 
 import poo.br.com.alura.screenmatch.modelos.Filme;
 import poo.br.com.alura.screenmatch.modelos.Serie;
+import poo.br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 
 public class PrincipalComListas {
+    private static ArrayList<Filme> listaDeFilmes;
     private static ArrayList<Filme> lista;
 
-    public static void main() {
-
+    public static void main(String [] args) {
         Filme meuFilme = new Filme("Norbit", 2000);
-        Serie supernatural = new Serie("Supernatural", 2002);
+        meuFilme.avalia(9);
         Filme outroFilme = new Filme("Avatar", 2022);
+        outroFilme.avalia(6);
         Filme filmeDoPaulo = new Filme("dogville", 1970);
+        filmeDoPaulo.avalia(10);
+        Serie supernatural = new Serie("Supernatural", 2002);
 
-        ArrayList<Filme> lista = new ArrayList<>();
+
+        ArrayList<Titulo> lista = new ArrayList<>();
         lista.add(filmeDoPaulo);
         lista.add(outroFilme);
         lista.add(meuFilme);
+        lista.add(supernatural);
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+
+            Filme filme = (Filme) item;
+            System.out.println("Classificação: " + filme.getClassificacao()); //Para usar o getClassificação de Filme.java in
+        }
     }
 }
